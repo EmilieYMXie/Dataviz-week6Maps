d10e6a0ddd8a72ea7cd5ace3ebfbf0b8514c2223
EmilieYMXie <ex2112@columbia.edu>
2015-03-04 20:29:02 -0500

    Add in for loop for markers and popup

--- scripts.js
+++ scripts.js
@@ -1,21 +1,33 @@
 /*
  * Taken from http://leafletjs.com/examples/quick-start.html
  * x, y longditude + latitude  40.81008,-73.95709 is 110 Morningside Drive
+ * GREL for finding lat: value.parseJson().results[0].geometry.location.lat
  */
 
-var map = L.map('leafletMap').setView([40.81008,-73.95709], 13);
+var map = L.map('leafletMap').setView([40.81008, -73.95709], 13);
 
 // add an OpenStreetMap tile layer
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+	attribution : '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-var marker = L.marker([40.81008,-73.95709]).addTo(map);
-
-var circle = L.circle([40.81008,-73.95709], 500, {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5
+var circle = L.circle([40.81008, -73.95709], 500, {
+	color : 'red',
+	fillColor : '#f03',
+	fillOpacity : 0.5
 }).addTo(map);
 
-marker.bindPopup("<b>University Apartment Housing</b><br>110 Morningside Drive.").openPopup();
+var warmingShelters = warmingSheltersData.rows;
+
+for (var i = 0; i < warmingShelters.length; i++) {
+	/*
+	 * Marker
+	 */
+	var marker = L.marker([40.81008, -73.95709]).addTo(map);
+
+	/*
+	 * Popup
+	 */
+	marker.bindPopup("<b>University Apartment Housing</b><br>110 Morningside Drive.").openPopup();
+}//end of for loop
+
