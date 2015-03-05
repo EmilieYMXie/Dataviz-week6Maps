e82cd8684ac1404427a0af529491fad9b474ce3a
EmilieYMXie <ex2112@columbia.edu>
2015-03-04 19:18:24 -0500

    Copy var map from leftlet

---
+++ scripts.js
@@ -0,0 +1,6 @@
+/*
+ * Taken from http://leafletjs.com/examples/quick-start.html
+ * x, y longditude + latitude  40.81008,-73.95709 is 110 Morningside Drive
+ */
+
+var map = L.map('leafletMap').setView([ 40.81008,-73.95709], 13);
