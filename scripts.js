f9f5022f26446ee1d983e45b18eff752f624b72a
EmilieYMXie <ex2112@columbia.edu>
2015-03-04 19:31:33 -0500

    Put in marker, circle, and text bubble

--- scripts.js
+++ scripts.js
@@ -3,9 +3,19 @@
  * x, y longditude + latitude  40.81008,-73.95709 is 110 Morningside Drive
  */
 
-var map = L.map('leafletMap').setView([ 40.81008,-73.95709], 13);
+var map = L.map('leafletMap').setView([40.81008,-73.95709], 13);
 
 // add an OpenStreetMap tile layer
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+
+var marker = L.marker([40.81008,-73.95709]).addTo(map);
+
+var circle = L.circle([40.81008,-73.95709], 500, {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5
+}).addTo(map);
+
+marker.bindPopup("<b>University Apartment Housing</b><br>110 Morningside Drive.").openPopup();
