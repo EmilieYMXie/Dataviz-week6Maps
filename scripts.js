c0494482370558bdf152e3149e4119a190f1b794
EmilieYMXie <ex2112@columbia.edu>
2015-03-04 19:23:21 -0500

    Copied Openstreetmap tile layer

--- scripts.js
+++ scripts.js
@@ -4,3 +4,8 @@
  */
 
 var map = L.map('leafletMap').setView([ 40.81008,-73.95709], 13);
+
+// add an OpenStreetMap tile layer
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
